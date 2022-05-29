ae211dae668b355ca154d72f5d3dada312b448dd
Kien Do <kienmarkdo@gmail.com>
2022-05-29 16:43:13 -0400

    Pushed project to remote repository

---
+++ README.txt
@@ -0,0 +1,12 @@
+From react JS Crash Course - Traversy Media
+
+`serve` localhost 8000 (serve -s build -p 8000)
+React's npm start will start localhost 3000 (npm start run)
+
+npm run start
+npm run build
+npm i -g serve
+serve -s build -p 8000
+
+npm run server
+npm start
--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
+  // ================= States =================
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]); // List of Tasks
+
+  useEffect(() => {
+
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+
+    getTasks();
+
+  }, []);
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+
+    return data;
+  }
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  }
+
+  // Delete Task
+  const deleteTask = async (id) => {
+    await fetch(`http:localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
+    setTasks(tasks.filter((task) => task.id !== id)); // show every task whose task.id is not equal to the parameter id
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task)); // if current task is the one the user clicked on, set the reminder to !task.reminder (the negation of its current state). Else set it as its previous state.
+  }
+
+  // =============================== return ===============================
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "Task list is empty. No tasks to display."}
     </div>
+
   );
 }
 
